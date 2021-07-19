12e982f2d5af4dca274afb3dcfa054d9de159c58
CL4P7R4P-97 <ashish.kumar98@outlook.com>
2021-07-19 14:22:12 +0530

    ComponentsPI

--- Coursera/src/app/app.module.ts
+++ Coursera/src/app/app.module.ts
@@ -5,19 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
-
+import { MatSliderModule } from '@angular/material/slider';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
    
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatSliderModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
