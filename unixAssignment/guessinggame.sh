818961371025003deb5d01a8476fb6feae6d14c5
CL4P7R4P-97 <ashish.kumar98@outlook.com>
2021-08-02 10:22:07 +0530

    GuessingGame assignment added

---
+++ unixAssignment/guessinggame.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#Filename: guessinggame.sh
+
+function userInput {
+	read response
+	echo "$response"
+}
+
+function fileCounter {
+	let number=$(echo $(ls) |wc -w)
+	echo "$number"
+}
+
+function startGame {
+	echo "How many files in the directory? "
+	echo "Enter the response and Press Enter"
+	
+}
+
+startGame
+guess=$(userInput)
+totalFiles=$(fileCounter)
+
+while [[ $guess -ne $totalFiles ]]
+do
+	if [[ $guess -gt $totalFiles ]]
+	then
+		echo "Too high!"
+		guess=$(userInput)
+	elif [[ $guess -lt $totalFiles ]]
+	then
+		echo "Too low"
+		guess=$(userInput)
+	fi
+done
+echo "Congratulations!!! You guessed it right"
