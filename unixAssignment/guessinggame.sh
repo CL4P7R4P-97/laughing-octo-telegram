27b03ca0d0c83447df9cad951f169917c059d461
CL4P7R4P-97 <ashish.kumar98@outlook.com>
2021-08-02 10:39:55 +0530

    Completed Assignment

--- unixAssignment/guessinggame.sh
+++ unixAssignment/guessinggame.sh
@@ -2,19 +2,19 @@
 #Filename: guessinggame.sh
 
 function userInput {
-	read response
-	echo "$response"
+        read response
+        echo "$response"
 }
 
 function fileCounter {
-	let number=$(echo $(ls) |wc -w)
-	echo "$number"
+        let number=$(echo $(ls) |wc -w)
+        echo "$number"
 }
 
 function startGame {
-	echo "How many files in the directory? "
-	echo "Enter the response and Press Enter"
-	
+        echo "How many files in the directory? "
+        echo "Enter the response and Press Enter"
+
 }
 
 startGame
@@ -23,14 +23,15 @@ totalFiles=$(fileCounter)
 
 while [[ $guess -ne $totalFiles ]]
 do
-	if [[ $guess -gt $totalFiles ]]
-	then
-		echo "Too high!"
-		guess=$(userInput)
-	elif [[ $guess -lt $totalFiles ]]
-	then
-		echo "Too low"
-		guess=$(userInput)
-	fi
+        if [[ $guess -gt $totalFiles ]]
+        then
+                echo "Too high!"
+                guess=$(userInput)
+        elif [[ $guess -lt $totalFiles ]]
+        then
+                echo "Too low"
+                guess=$(userInput)
+        fi
 done
 echo "Congratulations!!! You guessed it right"
+
